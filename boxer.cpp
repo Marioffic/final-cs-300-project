1a6eb40b1c8f0c5d7e44aff8dce3f71c602fb666
Marion Office <87404335+Marioffic@users.noreply.github.com>
2024-12-04 19:52:12 -0600

    added constructor, created logic for anger and appreciation meter

--- boxer.cpp
+++ boxer.cpp
@@ -17,4 +17,23 @@ private:
     int angerMeter;
     int appreciationMeter;
     std::string punchType;
-}
+public:
+    Boxer(const std::string& name, Type type, const std::string& punchType)
+        : name(name), type(type), angerMeter(0), appreciationMeter(0), punchType(punchType) {}
+
+    const std::string& getName() const { return name; }
+    Type getType() const { return type; }
+    int getAngerMeter() const { return angerMeter; }
+    int getAppreciationMeter() const { return appreciationMeter; }
+    const std::string& getPunchType() const { return punchType; }
+
+    void increaseAnger(int amount) {
+        angerMeter += amount;
+        if (angerMeter > 100) angerMeter = 100;
+    }
+
+    void increaseAppreciation(int amount) {
+        appreciationMeter += amount;
+        if (appreciationMeter > 100) appreciationMeter = 100;
+    }
+};
